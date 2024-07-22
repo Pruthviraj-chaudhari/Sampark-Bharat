a871a83791f42fcfaa3d548f66c68d493adf8b65
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2024-07-23 01:53:21 +0530

    Add new link

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,6 +9,7 @@ const Home = () => {
                 <div className="text-center"> {/* Center content horizontally */}
                     <h1 className="text-3xl font-bold mb-4">Home</h1> {/* Adjust font size and margin as needed */}
                     <h2>Go to: <span className="text-blue-600 underline"><a href="/rajabhau-waje">https://samparkbharat.vercel.app/rajabhau-waje</a></span></h2>
+                    <h2>Go to: <span className="text-blue-600 underline"><a href="/narendra-modi">https://samparkbharat.vercel.app/narendra-modi</a></span></h2>
                 </div>
             </main>
             <Footer />
