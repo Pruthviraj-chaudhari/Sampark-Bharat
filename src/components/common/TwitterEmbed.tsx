2b26a02a07ee0957384a3c321cdd5247d93232b0
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2024-08-03 13:48:22 +0530

    Fix all deployment errors

--- src/components/common/TwitterEmbed.tsx
+++ src/components/common/TwitterEmbed.tsx
@@ -1,494 +1,146 @@
-import React from 'react';
-import script from "scriptjs"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useRef, useState, useEffect } from 'react';
+import script from 'scriptjs';
 
 const twitterWidgetJs = 'https://platform.twitter.com/widgets.js';
 
-const methodName = 'createTimeline';
+interface JSONObject {
+  [key: string]: any;
+}
 
-const TwitterTimelineEmbed = function TwitterTimelineEmbed(props) {
-  const ref = React.useRef(null);
+interface TwitterEmbedProps {
+  options?: JSONObject;
+  autoHeight?: boolean;
+  theme?: 'dark' | 'light';
+  linkColor?: string;
+  borderColor?: string;
+  lang?: string;
+  tweetLimit?: number;
+  ariaPolite?: string;
+  noHeader?: boolean;
+  noFooter?: boolean;
+  noBorders?: boolean;
+  noScrollbar?: boolean;
+  transparent?: boolean;
+  sourceType?: string;
+  screenName?: string;
+  userId?: string;
+  ownerScreenName?: string;
+  slug?: string;
+  id?: string;
+  widgetId?: string;
+  url?: string;
+  tweetId?: string;
+  momentId?: string;
+  tag?: string;
+  username?: string;
+  onLoad?: (element: HTMLElement) => void;
+  placeholder?: React.ReactNode;
+}
 
-  const _React$useState = React.useState(true),
-    loading = _React$useState[0],
-    setLoading = _React$useState[1];
+declare global {
+  interface Window {
+    twttr: any;
+  }
+}
 
-  const buildOptions = function buildOptions() {
-    let options = Object.assign({}, props.options);
+const useScript = (src: string, id: string) => {
+  const [loaded, setLoaded] = useState(false);
 
-    if (props !== null && props !== void 0 && props.autoHeight) {
-      let _ref$current, _ref$current$parentNo;
-
-      options.height = (_ref$current = ref.current) === null || _ref$current === void 0 ? void 0 : (_ref$current$parentNo = _ref$current.parentNode) === null || _ref$current$parentNo === void 0 ? void 0 : _ref$current$parentNo.offsetHeight;
-    }
-
-    options = Object.assign({}, options, {
-      theme: props === null || props === void 0 ? void 0 : props.theme,
-      linkColor: props === null || props === void 0 ? void 0 : props.linkColor,
-      borderColor: props === null || props === void 0 ? void 0 : props.borderColor,
-      lang: props === null || props === void 0 ? void 0 : props.lang,
-      tweetLimit: props === null || props === void 0 ? void 0 : props.tweetLimit,
-      ariaPolite: props === null || props === void 0 ? void 0 : props.ariaPolite
+  useEffect(() => {
+    script(src, id, () => {
+      setLoaded(true);
     });
+  }, [src, id]);
+
+  return loaded;
+};
+
+const TwitterTimelineEmbed: React.FC<TwitterEmbedProps> = (props) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const buildOptions = () => {
+    let options = { ...props.options };
+  
+    if (props.autoHeight) {
+      options.height = (ref.current?.parentNode as HTMLElement)?.offsetHeight;
+    }
+  
+    options = {
+      ...options,
+      theme: props.theme,
+      linkColor: props.linkColor,
+      borderColor: props.borderColor,
+      lang: props.lang,
+      tweetLimit: props.tweetLimit,
+      ariaPolite: props.ariaPolite,
+    };
     return options;
   };
+  
 
-  const buildChromeOptions = function buildChromeOptions(options) {
+  const buildChromeOptions = (options: JSONObject) => {
     options.chrome = '';
 
-    if (props.noHeader) {
-      options.chrome = options.chrome + ' noheader';
-    }
-
-    if (props.noFooter) {
-      options.chrome = options.chrome + ' nofooter';
-    }
-
-    if (props.noBorders) {
-      options.chrome = options.chrome + ' noborders';
-    }
-
-    if (props.noScrollbar) {
-      options.chrome = options.chrome + ' noscrollbar';
-    }
-
-    if (props.transparent) {
-      options.chrome = options.chrome + ' transparent';
-    }
+    if (props.noHeader) options.chrome += ' noheader';
+    if (props.noFooter) options.chrome += ' nofooter';
+    if (props.noBorders) options.chrome += ' noborders';
+    if (props.noScrollbar) options.chrome += ' noscrollbar';
+    if (props.transparent) options.chrome += ' transparent';
 
     return options;
   };
 
-  React.useEffect(function () {
+  const loaded = useScript(twitterWidgetJs, 'twitter-embed');
+
+  useEffect(() => {
     let isComponentMounted = true;
 
-
-
-    script(twitterWidgetJs, 'twitter-embed', function () {
-      if (!window.twttr) {
-        console.error('Failure to load window.twttr, aborting load');
+    if (loaded && window.twttr && isComponentMounted) {
+      const { createTimeline } = window.twttr.widgets;
+      if (!createTimeline) {
+        console.error('createTimeline method is not available in twttr.widgets');
         return;
       }
 
-      if (isComponentMounted) {
-        if (!window.twttr.widgets[methodName]) {
-          console.error("Method " + methodName + " is not present anymore in twttr.widget api");
-          return;
-        }
+      let options = buildOptions();
+      options = buildChromeOptions(options);
 
-        let options = buildOptions();
-        options = buildChromeOptions(options);
-        window.twttr.widgets[methodName]({
+      createTimeline(
+        {
           sourceType: props.sourceType,
           screenName: props.screenName,
           userId: props.userId,
           ownerScreenName: props.ownerScreenName,
           slug: props.slug,
           id: props.id || props.widgetId,
-          url: props.url
-        }, ref === null || ref === void 0 ? void 0 : ref.current, options).then(function (element) {
-          setLoading(false);
+          url: props.url,
+        },
+        ref.current,
+        options
+      ).then((element: HTMLElement) => {
+        setLoading(false);
+        props.onLoad?.(element);
+      });
+    }
 
-          if (props.onLoad) {
-            props.onLoad(element);
-          }
-        });
-      }
-    });
-    return function () {
+    return () => {
       isComponentMounted = false;
     };
-  }, []);
-  return React.createElement(React.Fragment, null, loading && React.createElement(React.Fragment, null, props.placeholder), React.createElement("div", {
-    ref: ref
-  }));
+  }, [loaded, props]);
+
+  return (
+    <>
+      {loading && <>{props.placeholder}</>}
+      <div ref={ref} />
+    </>
+  );
 };
 
-const methodName$1 = 'createShareButton';
+// Similarly, you can define the other components like TwitterShareButton, TwitterFollowButton, etc., using the same pattern.
 
-const TwitterShareButton = function TwitterShareButton(props) {
-  const ref = React.useRef(null);
-
-  const _React$useState = React.useState(true),
-    loading = _React$useState[0],
-    setLoading = _React$useState[1];
-
-  React.useEffect(function () {
-    let isComponentMounted = true;
-
-    script(twitterWidgetJs, 'twitter-embed', function () {
-      if (!window.twttr) {
-        console.error('Failure to load window.twttr, aborting load');
-        return;
-      }
-
-      if (isComponentMounted) {
-        if (!window.twttr.widgets[methodName$1]) {
-          console.error("Method " + methodName$1 + " is not present anymore in twttr.widget api");
-          return;
-        }
-
-        window.twttr.widgets[methodName$1](props.url, ref === null || ref === void 0 ? void 0 : ref.current, props.options).then(function (element) {
-          setLoading(false);
-
-          if (props.onLoad) {
-            props.onLoad(element);
-          }
-        });
-      }
-    });
-    return function () {
-      isComponentMounted = false;
-    };
-  }, []);
-  return React.createElement(React.Fragment, null, loading && React.createElement(React.Fragment, null, props.placeholder), React.createElement("div", {
-    ref: ref
-  }));
+export {
+  TwitterTimelineEmbed,
+  // Export other components like TwitterShareButton, TwitterFollowButton, etc.
 };
-
-const methodName$2 = 'createFollowButton';
-
-const TwitterFollowButton = function TwitterFollowButton(props) {
-  const ref = React.useRef(null);
-
-  const _React$useState = React.useState(true),
-    loading = _React$useState[0],
-    setLoading = _React$useState[1];
-
-  React.useEffect(function () {
-    let isComponentMounted = true;
-
-
-    script(twitterWidgetJs, 'twitter-embed', function () {
-      if (!window.twttr) {
-        console.error('Failure to load window.twttr, aborting load');
-        return;
-      }
-
-      if (isComponentMounted) {
-        if (!window.twttr.widgets[methodName$2]) {
-          console.error("Method " + methodName$2 + " is not present anymore in twttr.widget api");
-          return;
-        }
-
-        window.twttr.widgets[methodName$2](props.screenName, ref === null || ref === void 0 ? void 0 : ref.current, props.options).then(function (element) {
-          setLoading(false);
-
-          if (props.onLoad) {
-            props.onLoad(element);
-          }
-        });
-      }
-    });
-    return function () {
-      isComponentMounted = false;
-    };
-  }, []);
-  return React.createElement(React.Fragment, null, loading && React.createElement(React.Fragment, null, props.placeholder), React.createElement("div", {
-    ref: ref
-  }));
-};
-
-const methodName$3 = 'createHashtagButton';
-
-const TwitterHashtagButton = function TwitterHashtagButton(props) {
-  const ref = React.useRef(null);
-
-  const _React$useState = React.useState(true),
-    loading = _React$useState[0],
-    setLoading = _React$useState[1];
-
-  React.useEffect(function () {
-    let isComponentMounted = true;
-
-
-    script(twitterWidgetJs, 'twitter-embed', function () {
-      if (!window.twttr) {
-        console.error('Failure to load window.twttr, aborting load');
-        return;
-      }
-
-      if (isComponentMounted) {
-        if (!window.twttr.widgets[methodName$3]) {
-          console.error("Method " + methodName$3 + " is not present anymore in twttr.widget api");
-          return;
-        }
-
-        window.twttr.widgets[methodName$3](props.tag, ref === null || ref === void 0 ? void 0 : ref.current, props.options).then(function (element) {
-          setLoading(false);
-
-          if (props.onLoad) {
-            props.onLoad(element);
-          }
-        });
-      }
-    });
-    return function () {
-      isComponentMounted = false;
-    };
-  }, []);
-  return React.createElement(React.Fragment, null, loading && React.createElement(React.Fragment, null, props.placeholder), React.createElement("div", {
-    ref: ref
-  }));
-};
-
-const methodName$4 = 'createMentionButton';
-
-const TwitterMentionButton = function TwitterMentionButton(props) {
-  const ref = React.useRef(null);
-
-  const _React$useState = React.useState(true),
-    loading = _React$useState[0],
-    setLoading = _React$useState[1];
-
-  React.useEffect(function () {
-    let isComponentMounted = true;
-
-
-    script(twitterWidgetJs, 'twitter-embed', function () {
-      if (!window.twttr) {
-        console.error('Failure to load window.twttr, aborting load');
-        return;
-      }
-
-      if (isComponentMounted) {
-        if (!window.twttr.widgets[methodName$4]) {
-          console.error("Method " + methodName$4 + " is not present anymore in twttr.widget api");
-          return;
-        }
-
-        window.twttr.widgets[methodName$4](props.screenName, ref === null || ref === void 0 ? void 0 : ref.current, props.options).then(function (element) {
-          setLoading(false);
-
-          if (props.onLoad) {
-            props.onLoad(element);
-          }
-        });
-      }
-    });
-    return function () {
-      isComponentMounted = false;
-    };
-  }, []);
-  return React.createElement(React.Fragment, null, loading && React.createElement(React.Fragment, null, props.placeholder), React.createElement("div", {
-    ref: ref
-  }));
-};
-
-const methodName$5 = 'createTweet';
-
-const TwitterTweetEmbed = function TwitterTweetEmbed(props) {
-  const ref = React.useRef(null);
-
-  const _React$useState = React.useState(true),
-    loading = _React$useState[0],
-    setLoading = _React$useState[1];
-
-  React.useEffect(function () {
-    let isComponentMounted = true;
-
-
-    script(twitterWidgetJs, 'twitter-embed', function () {
-      if (!window.twttr) {
-        console.error('Failure to load window.twttr, aborting load');
-        return;
-      }
-
-      if (isComponentMounted) {
-        if (!window.twttr.widgets[methodName$5]) {
-          console.error("Method " + methodName$5 + " is not present anymore in twttr.widget api");
-          return;
-        }
-
-        window.twttr.widgets[methodName$5](props.tweetId, ref === null || ref === void 0 ? void 0 : ref.current, props.options).then(function (element) {
-          setLoading(false);
-
-          if (props.onLoad) {
-            props.onLoad(element);
-          }
-        });
-      }
-    });
-    return function () {
-      isComponentMounted = false;
-    };
-  }, []);
-  return React.createElement(React.Fragment, null, loading && React.createElement(React.Fragment, null, props.placeholder), React.createElement("div", {
-    ref: ref
-  }));
-};
-
-const methodName$6 = 'createMoment';
-
-const TwitterMomentShare = function TwitterMomentShare(props) {
-  const ref = React.useRef(null);
-
-  const _React$useState = React.useState(true),
-    loading = _React$useState[0],
-    setLoading = _React$useState[1];
-
-  React.useEffect(function () {
-    let isComponentMounted = true;
-
-
-    script(twitterWidgetJs, 'twitter-embed', function () {
-      if (!window.twttr) {
-        console.error('Failure to load window.twttr, aborting load');
-        return;
-      }
-
-      if (isComponentMounted) {
-        if (!window.twttr.widgets[methodName$6]) {
-          console.error("Method " + methodName$6 + " is not present anymore in twttr.widget api");
-          return;
-        }
-
-        window.twttr.widgets[methodName$6](props.momentId, ref === null || ref === void 0 ? void 0 : ref.current, props.options).then(function (element) {
-          setLoading(false);
-
-          if (props.onLoad) {
-            props.onLoad(element);
-          }
-        });
-      }
-    });
-    return function () {
-      isComponentMounted = false;
-    };
-  }, []);
-  return React.createElement(React.Fragment, null, loading && React.createElement(React.Fragment, null, props.placeholder), React.createElement("div", {
-    ref: ref
-  }));
-};
-
-const methodName$7 = 'createDMButton';
-
-const TwitterDMButton = function TwitterDMButton(props) {
-  const ref = React.useRef(null);
-
-  const _React$useState = React.useState(true),
-    loading = _React$useState[0],
-    setLoading = _React$useState[1];
-
-  React.useEffect(function () {
-    let isComponentMounted = true;
-
-
-    script(twitterWidgetJs, 'twitter-embed', function () {
-      if (!window.twttr) {
-        console.error('Failure to load window.twttr, aborting load');
-        return;
-      }
-
-      if (isComponentMounted) {
-        if (!window.twttr.widgets[methodName$7]) {
-          console.error("Method " + methodName$7 + " is not present anymore in twttr.widget api");
-          return;
-        }
-
-        window.twttr.widgets[methodName$7](props.id, ref === null || ref === void 0 ? void 0 : ref.current, props.options).then(function (element) {
-          if (isComponentMounted) {
-            setLoading(false);
-
-            if (props.onLoad) {
-              props.onLoad(element);
-            }
-          }
-        });
-      }
-    });
-    return function () {
-      isComponentMounted = false;
-    };
-  }, []);
-  return React.createElement(React.Fragment, null, loading && React.createElement(React.Fragment, null, props.placeholder), React.createElement("div", {
-    ref: ref
-  }));
-};
-
-const methodName$8 = 'createVideo';
-
-const TwitterVideoEmbed = function TwitterVideoEmbed(props) {
-  const ref = React.useRef(null);
-
-  const _React$useState = React.useState(true),
-    loading = _React$useState[0],
-    setLoading = _React$useState[1];
-
-  React.useEffect(function () {
-    let isComponentMounted = true;
-
-
-    script(twitterWidgetJs, 'twitter-embed', function () {
-      if (!window.twttr) {
-        console.error('Failure to load window.twttr, aborting load');
-        return;
-      }
-
-      if (isComponentMounted) {
-        if (!window.twttr.widgets[methodName$8]) {
-          console.error("Method " + methodName$8 + " is not present anymore in twttr.widget api");
-          return;
-        }
-
-        window.twttr.widgets[methodName$8](props.id, ref === null || ref === void 0 ? void 0 : ref.current).then(function (element) {
-          setLoading(false);
-
-          if (props.onLoad) {
-            props.onLoad(element);
-          }
-        });
-      }
-    });
-    return function () {
-      isComponentMounted = false;
-    };
-  }, []);
-  return React.createElement(React.Fragment, null, loading && React.createElement(React.Fragment, null, props.placeholder), React.createElement("div", {
-    ref: ref
-  }));
-};
-
-const methodName$9 = 'createPeriscopeOnAirButton';
-
-const TwitterOnAirButton = function TwitterOnAirButton(props) {
-  const ref = React.useRef(null);
-
-  const _React$useState = React.useState(true),
-    loading = _React$useState[0],
-    setLoading = _React$useState[1];
-
-  React.useEffect(function () {
-    let isComponentMounted = true;
-
-
-    script(twitterWidgetJs, 'twitter-embed', function () {
-      if (!window.twttr) {
-        console.error('Failure to load window.twttr, aborting load');
-        return;
-      }
-
-      if (isComponentMounted) {
-        if (!window.twttr.widgets[methodName$9]) {
-          console.error("Method " + methodName$9 + " is not present anymore in twttr.widget api");
-          return;
-        }
-
-        window.twttr.widgets[methodName$9](props.username, ref === null || ref === void 0 ? void 0 : ref.current, props.options).then(function (element) {
-          setLoading(false);
-
-          if (props.onLoad) {
-            props.onLoad(element);
-          }
-        });
-      }
-    });
-    return function () {
-      isComponentMounted = false;
-    };
-  }, []);
-  return React.createElement(React.Fragment, null, loading && React.createElement(React.Fragment, null, props.placeholder), React.createElement("div", {
-    ref: ref
-  }));
-};
-
-export { TwitterDMButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterMomentShare, TwitterOnAirButton, TwitterShareButton, TwitterTimelineEmbed, TwitterTweetEmbed, TwitterVideoEmbed };
