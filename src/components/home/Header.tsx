bb33106c5f276b64e5b16779ea2cdb24b7146766
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2024-10-02 10:54:48 +0530

    Added CAPTCHA

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -21,19 +21,19 @@ const Header = () => {
                         <span className="sr-only">Acme Inc</span>
                     </a>
                     <div className="grid gap-2 py-6">
-                        <a href="#" className="flex w-full items-center py-2 text-lg font-semibold">
+                        <a href="#home" className="flex w-full items-center py-2 text-lg font-semibold">
                             Home
                         </a>
-                        <a href="#" className="flex w-full items-center py-2 text-lg font-semibold">
+                        <a href="#features" className="flex w-full items-center py-2 text-lg font-semibold">
+                            Features
+                        </a>
+                        <a href="#pricing" className="flex w-full items-center py-2 text-lg font-semibold">
+                            Pricing
+                        </a>
+                        <a href="#about" className="flex w-full items-center py-2 text-lg font-semibold">
                             About
                         </a>
-                        <a href="#" className="flex w-full items-center py-2 text-lg font-semibold">
-                            Services
-                        </a>
-                        <a href="#" className="flex w-full items-center py-2 text-lg font-semibold">
-                            Portfolio
-                        </a>
-                        <a href="#" className="flex w-full items-center py-2 text-lg font-semibold">
+                        <a href="#contact" className="flex w-full items-center py-2 text-lg font-semibold">
                             Contact
                         </a>
                     </div>
@@ -86,7 +86,7 @@ const Header = () => {
                     </NavigationMenuLink>
                     <NavigationMenuLink asChild>
                         <a
-                            href="#footer"
+                            href="#contact"
                             className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-lg font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
 
                         >
